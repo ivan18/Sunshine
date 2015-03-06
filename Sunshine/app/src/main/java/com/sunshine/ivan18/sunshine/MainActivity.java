51f45114b0a629972faaf5c217a9e49f4f61c32f
ivan18 <ivan.bruno18@gmail.com>
2015-03-06 17:47:31 +0100

    Created ListView and FrameLayout

--- Sunshine/app/src/main/java/com/sunshine/ivan18/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/sunshine/ivan18/sunshine/MainActivity.java
@@ -1,9 +1,20 @@
 package com.sunshine.ivan18.sunshine;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +23,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            PlaceholderFragment frag_login= new PlaceholderFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            //firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, frag_login).commit();
+        }
     }
 
 
@@ -35,5 +68,41 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+      * A placeholder fragment containing a single view.
+      */
+    public static class PlaceholderFragment extends Fragment{
+        private ArrayAdapter<String> adapter;
+        public PlaceholderFragment(){
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View rootView= inflater.inflate(R.layout.fragment_main, container, false);
+            List<String> itemsWeather=populateWeather();
+
+            adapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textView,itemsWeather);
+            ListView listView= (ListView)rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(adapter);
+            return rootView;
+        }
+    }
+
+    public static List<String> populateWeather(){
+        List<String> items = new ArrayList<String>();
+        String[] days= {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        String[] weathers={"Sun","Cloudy","Rain","Snow"};
+        String itemContent= null;
+        Random rnd= new Random();
+        for (int i=0; i<20; i++){
+            int rand=(i)%7;
+            itemContent=days[rand]+" - ";
+            itemContent+=weathers[rnd.nextInt(3)];
+            items.add(itemContent);
+        }
+        return items;
     }
 }
